13ce04042adf128df42df650e42193e66f7f58df
이지환 <zncnf@naver.com>
2020-01-20 04:25:57 +0900

    몬스터, 던전 추가 Monster/monster 폴더 내 파일 일괄 수정

--- Classes/MindForest_Stage1.cpp
+++ Classes/MindForest_Stage1.cpp
@@ -12,10 +12,15 @@ bool MindForest_Stage1::init() {
 
 	_hero = new Hero(this);
 	_heroControl = new HeroControl(this, _hero);
+	_dungeon = new Dungeon(this, 1000); //2번째 인자에 체력 넣음
 
 	this->schedule(schedule_selector(MindForest_Stage1::tick));
 	this->schedule(schedule_selector(MindForest_Stage1::HeroManaRegen), _hero->getManaRegenSpeed());
 	this->schedule(schedule_selector(MindForest_Stage1::HeroMeatRegen), _hero->getMeatRegenSpeed());
+
+	_monster.push_back(new Monster(this, _hero, Mob::좀비킹)); //몬스터 생성
+
+	_hero->getHero()->setZOrder(50); //위치조절을 위한 임시 설정입니당
 
 
 	return true;
@@ -25,6 +30,8 @@ void MindForest_Stage1::tick(float delta)
 {
 	_heroControl->HeroMove();
 	_heroControl->UnitMove();
+
+	MonsterTick();
 }
 
 void MindForest_Stage1::HeroManaRegen(float delta)
@@ -35,4 +42,24 @@ void MindForest_Stage1::HeroManaRegen(float delta)
 void MindForest_Stage1::HeroMeatRegen(float delta)
 {
 	_heroControl->HeroMeatRegen();
+}
+
+void MindForest_Stage1::MonsterTick()
+{
+	if (rand() % 150 == 0) {
+		_monster.push_back(new Monster(this, _hero, Mob::분홍미라));
+	}
+	for (int i = 0; i < _monster.size(); i++) {
+		if (_monster[i]->getIsSummon()) { //좀비킹 소환술
+			_monster.push_back(new Monster(this, _hero, Mob::걷는좀비));
+			_monster.back()->setSummunPositionX(_monster[i]->getIsSummon());
+			_monster.push_back(new Monster(this, _hero, Mob::걷는좀비));
+			_monster.back()->setSummunPositionX(_monster[i]->getIsSummon());
+		}
+		_monster[i]->MonsterMove();
+		if (_monster[i]->getIsRemove()) {
+			this->removeChild(_monster[i]->getMonster());
+			_monster.erase(_monster.begin() + i);
+		}
+	}
 }
--- Classes/MindForest_Stage1.h
+++ Classes/MindForest_Stage1.h
@@ -4,12 +4,24 @@
 #include "Hero.h"
 #include "HeroControl.h"
 #include "HeroUnit.h"
+#include "Monster.h"
+#include "Dungeon.h"
+
+enum ZORDER { //이거 어디두는게 좋을까여?
+	던전뒤 = 10,
+	영웅 = 50,
+	유닛 = 55,
+	던전앞 = 90
+};
 
 class MindForest_Stage1 : public Scene
 {
 	Hero* _hero;
 	HeroControl* _heroControl;
 	HeroUnit* _heroUnit;
+	vector<Monster*> _monster;
+	Dungeon* _dungeon;
+
 public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
@@ -18,4 +30,6 @@ public:
 	void tick(float delta);
 	void HeroManaRegen(float delta);
 	void HeroMeatRegen(float delta);
+
+	void MonsterTick();
 };
