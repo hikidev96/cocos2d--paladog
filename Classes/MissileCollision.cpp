a275b5dc53628d01e9919fe49994dd64d8362c2b
MeanTOT <dpalstn91@gmail.com>
2020-01-27 14:02:04 +0900

    Stage Unit Skill Update

--- Classes/MissileCollision.cpp
+++ Classes/MissileCollision.cpp
@@ -7,6 +7,7 @@ MissileCollision::MissileCollision(Layer* layer, Movement moveway, Vec2 pos, int
 
 	cache = SpriteFrameCache::getInstance();
 	cache->addSpriteFramesWithFile("Player/weapons/m01_1.plist"); // plist 추가
+	cache->addSpriteFramesWithFile("Player/weapons/eff_normal_01.plist"); // plist 추가
 
 	_MissileSprite = Sprite::createWithSpriteFrameName("m01_eff_b_0001.png");
 
@@ -39,6 +40,28 @@ MissileCollision::MissileCollision(Layer* layer, Movement moveway, Vec2 pos, int
 	_hammerSkillEffectRepeat_B1 = RepeatForever::create(_hammerSkillEffectAnimate_B1);
 	_hammerSkillEffectRepeat_B1->retain();
 	_hammerSkillEffectRepeat_B1->setTag(777);
+
+	_hammerHitEffectAnimation = Animation::create();
+	_hammerHitEffectAnimation->setDelayPerUnit(0.03f);
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0001.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0002.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0003.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0004.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0005.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0006.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0007.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0008.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0009.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0010.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0011.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0012.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0013.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0014.png"));
+	_hammerHitEffectAnimation->addSpriteFrame(cache->getSpriteFrameByName("eff_explosion_a_0015.png"));
+	_hammerHitEffectAnimate = Animate::create(_hammerHitEffectAnimation);
+	_hammerHitEffectAnimate->retain();
+	_hammerHitEffectAnimate->setTag(790);
+
 }
 
 void MissileCollision::BringMonsterVec(vector<Monster*> monster, Dungeon* dungeon)
@@ -66,8 +89,17 @@ void MissileCollision::BringMonsterVec(vector<Monster*> monster, Dungeon* dungeo
 		if (_MissileSprite->getPositionX() > monster[i]->getMonster()->getPositionX() - 30 &&
 			_MissileSprite->getPositionX() < monster[i]->getMonster()->getPositionX() + 10 && !_firstAtk)
 		{
+			// 히트시 잠시 빨간색으로 바뀌는효과
+			monster[i]->getMonster()->runAction(Sequence::createWithTwoActions(TintTo::create(0, Color3B::RED), TintTo::create(0.1f, Color3B::WHITE)));
+
 			monster[i]->Hit(_Atk);
 			_firstAtk = true;
+
+			_hitEffect1 = Sprite::createWithSpriteFrameName("eff_explosion_a_0001.png");
+			_hitEffect1->setOpacity(255);
+			_layer->addChild(_hitEffect1, monster[i]->getMonster()->getZOrder());
+			_hitEffect1->setPosition(monster[i]->getMonster()->getPosition().x, monster[i]->getMonster()->getPosition().y - 30);
+			_hitEffect1->runAction(Sequence::create(_hammerHitEffectAnimate, RemoveSelf::create(true), nullptr)->clone());
 
 			if (_address1 == nullptr)
 			{
@@ -79,11 +111,20 @@ void MissileCollision::BringMonsterVec(vector<Monster*> monster, Dungeon* dungeo
 		if (_MissileSprite->getPositionX() > monster[i]->getMonster()->getPositionX() - 30 &&
 			_MissileSprite->getPositionX() < monster[i]->getMonster()->getPositionX() + 10 && _firstAtk && !_secondAtk)
 		{
+			// 히트시 잠시 빨간색으로 바뀌는효과
+			monster[i]->getMonster()->runAction(Sequence::createWithTwoActions(TintTo::create(0, Color3B::RED), TintTo::create(0.1f, Color3B::WHITE)));
+
 			if (_address1 == monster[i]->getMonster())
 				continue;
 
 			monster[i]->Hit(_Atk);
 			_secondAtk = true;
+
+			_hitEffect2 = Sprite::createWithSpriteFrameName("eff_explosion_a_0001.png");
+			_hitEffect2->setOpacity(255);
+			_layer->addChild(_hitEffect2, monster[i]->getMonster()->getZOrder());
+			_hitEffect2->setPosition(monster[i]->getMonster()->getPosition().x, monster[i]->getMonster()->getPosition().y - 30);
+			_hitEffect2->runAction(Sequence::create(_hammerHitEffectAnimate, RemoveSelf::create(true), nullptr)->clone());
 
 			if (_address2 == nullptr)
 			{
@@ -95,10 +136,19 @@ void MissileCollision::BringMonsterVec(vector<Monster*> monster, Dungeon* dungeo
 		if (_MissileSprite->getPositionX() > monster[i]->getMonster()->getPositionX() - 30 &&
 			_MissileSprite->getPositionX() < monster[i]->getMonster()->getPositionX() + 10 && _firstAtk && _secondAtk)
 		{
+			// 히트시 잠시 빨간색으로 바뀌는효과
+			monster[i]->getMonster()->runAction(Sequence::createWithTwoActions(TintTo::create(0, Color3B::RED), TintTo::create(0.1f, Color3B::WHITE)));
+
 			if (_address2 == monster[i]->getMonster() || _address1 == monster[i]->getMonster())
 				continue;
 
 			monster[i]->Hit(_Atk);
+
+			_hitEffect3 = Sprite::createWithSpriteFrameName("eff_explosion_a_0001.png");
+			_hitEffect3->setOpacity(255);
+			_layer->addChild(_hitEffect3, monster[i]->getMonster()->getZOrder());
+			_hitEffect3->setPosition(monster[i]->getMonster()->getPosition().x, monster[i]->getMonster()->getPosition().y - 30);
+			_hitEffect3->runAction(Sequence::create(_hammerHitEffectAnimate, RemoveSelf::create(true), nullptr)->clone());
 
 			// 객체 삭제
 			_erase = true;
@@ -109,7 +159,17 @@ void MissileCollision::BringMonsterVec(vector<Monster*> monster, Dungeon* dungeo
 	//몬스터 기지를 공격
 	if (dungeon->getMonsterBase()->getPositionX() - _MissileSprite->getPositionX() + 50<= 0)
 	{
+		// 히트시 잠시 빨간색으로 바뀌는효과
+		dungeon->getMonsterBase()->runAction(Sequence::createWithTwoActions(TintTo::create(0, Color3B::RED), TintTo::create(0.1f, Color3B::WHITE)));
+
 		dungeon->Hit(_Atk);
+
+		_hitEffect4 = Sprite::createWithSpriteFrameName("eff_explosion_a_0001.png");
+		_hitEffect4->setScale(2.f);
+		_layer->addChild(_hitEffect4, dungeon->getMonsterBase()->getZOrder() + 1);
+		_hitEffect4->setPosition(dungeon->getMonsterBase()->getPosition().x + 100, dungeon->getMonsterBase()->getPosition().y + 100);
+		_hitEffect4->runAction(Sequence::create(_hammerHitEffectAnimate, RemoveSelf::create(true), nullptr)->clone());
+
 		_erase = true;
 		_MissileSprite->runAction(RemoveSelf::create(true));
 	}
--- Classes/MissileCollision.h
+++ Classes/MissileCollision.h
@@ -24,12 +24,20 @@ private:
 	Sprite* _address1;
 	Sprite* _address2;
 
+	Sprite* _hitEffect1;
+	Sprite* _hitEffect2;
+	Sprite* _hitEffect3;
+	Sprite* _hitEffect4;
+
 	float _Atk;
 	float _speed;
 
 	Animation* _hammerSkillEffectAnimation_B1; // 고무망치 스킬 이펙트B
 	Animate* _hammerSkillEffectAnimate_B1; // 고무망치 스킬 이펙트B
 	RepeatForever* _hammerSkillEffectRepeat_B1; // 고무망치 스킬 이펙트B
+
+	Animation* _hammerHitEffectAnimation; // 고무망치 히트 이펙트
+	Animate* _hammerHitEffectAnimate; // 고무망치 히트 이펙트
 public:
 	MissileCollision(Layer* layer, Movement moveway, Vec2 pos, int zoder);
 
