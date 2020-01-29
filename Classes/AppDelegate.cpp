adfd9ef97f89c5069912429462b654c75a95ade9
chanHyung <ych7305@naver.com>
2020-01-29 16:42:31 +0900

    몬스터 밸런스

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -75,7 +75,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
   glview->setDesignResolutionSize(480, 320, ResolutionPolicy::EXACT_FIT);
 
   // create a scene. it's an autorelease object
-  auto scene = MindForest_Stage1::createScene();
+  auto scene = MindForest_Stage2::createScene();
 
   // run
   director->runWithScene(scene);
